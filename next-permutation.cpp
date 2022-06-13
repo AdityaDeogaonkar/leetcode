20eb8aac7e0be23c565c71b0e09bd0733d03c465
AdityaDeogaonkar <64679715+AdityaDeogaonkar@users.noreply.github.com>
2022-06-13 12:08:05 +0530

    Create next-permutation.cpp

---
+++ next-permutation.cpp
@@ -0,0 +1,31 @@
+// ADITYA DEOGAONKAR //
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        
+        int index = -1;
+        int len = nums.size();
+        for(int i=len-1;i>0;i--) {
+            if(nums[i]>nums[i-1]) {
+                index = i;
+                break;
+            }
+        }
+        if(index == -1) {
+             reverse(nums.begin(),nums.end());
+        }
+        else {
+            
+        
+        int prev  = index;
+        for(int i=index+1;i<len;i++) {
+            if(nums[i]>nums[index-1] && nums[i]<=nums[prev]) {
+                prev = i;
+            }
+        }
+        swap(nums[index-1],nums[prev]);
+        reverse(nums.begin()+index,nums.end());
+        }
+    }
+};
