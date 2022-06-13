255802a1ba16847aaff367dff905122b2d1ce9d5
AdityaDeogaonkar <64679715+AdityaDeogaonkar@users.noreply.github.com>
2022-06-13 16:57:44 +0530

    Create rotate-matrix.cpp

---
+++ rotate-matrix.cpp
@@ -0,0 +1,25 @@
+// ADITYA DEOGAONKAR //
+
+MOST OPTIMIZED TC= O(N^2);
+SC = O(1);
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<i;j++) {
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }
+        
+        for(int i=0;i<n;i++) {
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
+
+
+// AUXILLARY MATRIX USED  TC = O(N^2)
+                          SC = O(N^2)
